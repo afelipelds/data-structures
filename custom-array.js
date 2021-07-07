37dab6c420da56f819383452875c13978709ac21
andres.eslava <andres.eslava@ticketmaster.com>
2021-07-06 21:48:37 -0500

    add/delete methods

--- custom-array.js
+++ custom-array.js
@@ -2,18 +2,54 @@ class MyArray {
   constructor() {
     this.length = 0;
     this.data = {};
-  }
+  };
 
   get(index) {
     return this.data[index];
-  }
+  };
 
   push(item) {
     // incrementando el valor con ++ luego del uso de this.length. 
     // 1ero: asignará el valor actual , seguido de 2do se incrementará en 1.
     this.data[this.length++] = item; 
     return this.data;
+  };
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  };
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftIndex(index);
+    return item;
+  };
+
+  shiftIndex(index) {
+    debugger;
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  };
+
+  shift() {
+    return this.delete(0);
   }
+
+  unshift(item) {
+    for (let i = this.length; i >= 1; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = item;
+    this.length++;
+    return this.data;
+  }
+  
 }
 
 const myArray = new MyArray();
